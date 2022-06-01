66a03ec119ef6b0693cb84cb3ef2c88a60af3026
Nirvana <gui1050214471@163.com>
2022-06-01 22:24:02 +0800

    netty 增加sync 的注释

--- example/src/main/java/io/netty/example/echo/EchoServer.java
+++ example/src/main/java/io/netty/example/echo/EchoServer.java
@@ -75,6 +75,7 @@ public final class EchoServer {
              });
 
             // Start the server.
+            // sync()方法会阻塞当前线程, 直到服务器启动完成
             ChannelFuture f = b.bind(PORT).sync();
 
             // Wait until the server socket is closed.
